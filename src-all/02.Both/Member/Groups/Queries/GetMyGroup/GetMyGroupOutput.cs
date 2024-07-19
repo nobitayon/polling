7cf17b9a1b3e4658957b5dfa68daae6d627e1547
nobitayon <yonrungrung@gmail.com>
2024-07-19 20:10:49 +0700

    Membuat logic untuk menampilkan detail group(data composite)
    - mendapat detail grup
    - mendapat daftar member dari grup
    - mendapat daftar polling dalam grup(belum pagination)

---
+++ src-all/02.Both/Member/Groups/Queries/GetMyGroup/GetMyGroupOutput.cs
@@ -0,0 +1,25 @@
+﻿using Delta.Polling.Base.Polls.Enums;
+
+namespace Delta.Polling.Both.Member.Groups.Queries.GetMyGroup;
+
+public record GetMyGroupOutput : Output<GroupItem>
+{
+}
+
+// TODO: apakah Id, Name, Created, CreatedBy dijadiin satu field 
+public record GroupItem
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+    public required DateTimeOffset Created { get; init; }
+    public required string CreatedBy { get; init; }
+    public IEnumerable<PollItem> PollItems { get; init; } = [];
+    public IEnumerable<string> MemberItems { get; init; } = [];
+}
+
+public record PollItem
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+    public required PollStatus Status { get; set; }
+}
---
+++ src-be/04.Logics/Member/Groups/Queries/GetMyGroup/GetMyGroupQuery.cs
@@ -0,0 +1,70 @@
+﻿using Delta.Polling.Both.Member.Groups.Queries.GetMyGroup;
+
+namespace Delta.Polling.Logics.Member.Groups.Queries.GetMyGroup;
+
+[Authorize(RoleName = RoleNameFor.Member)]
+public class GetMyGroupQuery : GetMyGroupRequest, IRequest<GetMyGroupOutput>
+{
+
+}
+
+public class GetMyGroupQueryValidator : AbstractValidator<GetMyGroupQuery>
+{
+    public GetMyGroupQueryValidator()
+    {
+        Include(new GetMyGroupRequestValidator());
+    }
+}
+
+public class GetMyGroupQueryHandler(
+    IDatabaseService databaseService,
+    ICurrentUserService currentUserService)
+    : IRequestHandler<GetMyGroupQuery, GetMyGroupOutput>
+{
+    public async Task<GetMyGroupOutput> Handle(GetMyGroupQuery request, CancellationToken cancellationToken)
+    {
+
+        var memberGroup = await databaseService.GroupMembers
+                        .Where(gm => gm.GroupId == request.GroupId)
+                        .Select(gm => gm.Username)
+                        .ToListAsync(cancellationToken);
+
+        var isInGroup = memberGroup
+            .Any(member =>
+            {
+                return member == currentUserService.Username;
+            });
+
+        if (!isInGroup)
+        {
+            throw new Exception($"You cannot access group with Id {request.GroupId} because you are not member of group");
+        }
+
+        var groupDetails = await databaseService.Groups
+                            .SingleOrDefaultAsync(group => group.Id == request.GroupId, cancellationToken)
+                            ?? throw new EntityNotFoundException("Group", request.GroupId);
+
+        // TODO: Belum ada pagination untuk yang ini. Mungkin nunggu kak fu hari senin
+        var listPolling = await databaseService.Polls
+                            .Where(poll => poll.GroupId == request.GroupId)
+                            .Select(poll => new PollItem
+                            {
+                                Id = poll.Id,
+                                Title = poll.Title,
+                                Status = poll.Status
+                            }).ToListAsync(cancellationToken);
+
+        var result = new GroupItem
+        {
+            Id = groupDetails.Id,
+            Name = groupDetails.Name,
+            Created = groupDetails.Created,
+            CreatedBy = groupDetails.CreatedBy,
+            MemberItems = memberGroup,
+            PollItems = listPolling
+        };
+
+        return new GetMyGroupOutput { Data = result };
+    }
+}
+
