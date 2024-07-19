073ee7ab8ba7e0e6569589c081e6a8d6a5f20d80
nobitayon <yonrungrung@gmail.com>
2024-07-19 17:27:05 +0700

    membuat logic members can view list of groups they belong to
    - pake pagination, tapi SortField-nya masih hardcode

---
+++ src-all/02.Both/Member/Groups/Queries/GetMyGroups/GetMyGroupsOutput.cs
@@ -0,0 +1,11 @@
+﻿namespace Delta.Polling.Both.Member.Groups.Queries.GetMyGroups;
+
+public record GetMyGroupsOutput : Output<IEnumerable<GroupItem>>
+{
+}
+
+public record GroupItem
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+}
