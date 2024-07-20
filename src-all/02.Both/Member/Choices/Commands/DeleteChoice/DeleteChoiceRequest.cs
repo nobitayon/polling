7f4b4c5fa2aa832a5c53cd2391f47449784cc8a8
nobitayon <yonrungrung@gmail.com>
2024-07-20 22:36:40 +0700

    membuat logic untuk menghapus choice dari polling dengan status draft
    #248

---
+++ src-all/02.Both/Member/Choices/Commands/DeleteChoice/DeleteChoiceRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Delta.Polling.Both.Member.Choices.Commands.DeleteChoice;
+
+public class DeleteChoiceRequest
+{
+    public required Guid PollId { get; init; }
+    public required Guid ChoiceId { get; init; }
+}
+
+public class DeleteChoiceRequestValidator : AbstractValidator<DeleteChoiceRequest>
+{
+    public DeleteChoiceRequestValidator()
+    {
+        _ = RuleFor(x => x.PollId)
+           .NotEmpty();
+
+        _ = RuleFor(x => x.ChoiceId)
+          .NotEmpty();
+    }
+}
