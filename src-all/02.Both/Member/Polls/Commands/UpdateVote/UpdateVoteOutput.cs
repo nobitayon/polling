26071184769dbfdf6bded13de76052883485d551
nobitayon <yonrungrung@gmail.com>
2024-07-22 04:49:19 +0700

    membuat logic untuk update vote
    - juga update pada add vote, pada bagian pengecekan "harus memilih opsi yang ditambah pada saat ongoing poll"
    
    #254
    #256

---
+++ src-all/02.Both/Member/Polls/Commands/UpdateVote/UpdateVoteOutput.cs
@@ -0,0 +1,10 @@
+﻿namespace Delta.Polling.Both.Member.Polls.Commands.UpdateVote;
+
+public record UpdateVoteOutput : Output<IEnumerable<AnswerItem>>
+{
+}
+
+public record AnswerItem
+{
+    public required Guid AnswerId { get; init; }
+}
---
+++ src-all/02.Both/Member/Polls/Commands/UpdateVote/UpdateVoteRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Delta.Polling.Both.Member.Polls.Commands.UpdateVote;
+
+public class UpdateVoteRequest
+{
+    public required Guid PollId { get; init; }
+    public required IEnumerable<Guid> ListChoice { get; init; }
+}
+
+public class UpdateVoteRequestValidator : AbstractValidator<UpdateVoteRequest>
+{
+    public UpdateVoteRequestValidator()
+    {
+        _ = RuleFor(x => x.PollId)
+           .NotEmpty();
+
+        _ = RuleFor(x => x.ListChoice)
+           .Must(x => x != null && x.Count() >= 1)
+           .WithMessage("ListChoice must contain more than one item.");
+    }
+}
