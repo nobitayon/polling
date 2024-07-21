bcb2540823f94750ea6f72abbbd6aa21fdd800c3
nobitayon <yonrungrung@gmail.com>
2024-07-21 08:29:55 +0700

    membuat logic untuk update poll
    - terdapat perubahan juga pada entity domain, mengenai set, init
    
    #250

---
+++ src-all/02.Both/Member/Polls/Commands/UpdatePoll/UpdatePollRequest.cs
@@ -0,0 +1,30 @@
+﻿namespace Delta.Polling.Both.Member.Polls.Commands.UpdatePoll;
+
+public class UpdatePollRequest
+{
+    public required Guid PollId { get; init; }
+    public required string Title { get; init; }
+    public required string Question { get; init; }
+    public required int MaximumAnswer { get; init; }
+    public required bool AllowOtherChoice { get; init; }
+}
+
+public class UpdatePollRequestValidator : AbstractValidator<UpdatePollRequest>
+{
+    public UpdatePollRequestValidator()
+    {
+        _ = RuleFor(x => x.PollId)
+           .NotEmpty();
+
+        _ = RuleFor(x => x.Title)
+            .NotEmpty()
+            .MaximumLength(PollsMaxLengthFor.Title);
+
+        _ = RuleFor(x => x.Question)
+           .NotEmpty()
+           .MaximumLength(PollsMaxLengthFor.Question);
+
+        _ = RuleFor(x => x.MaximumAnswer)
+            .InclusiveBetween(PollsMinValueFor.MaximumAnswer, PollsMaxValueFor.MaximumAnswer);
+    }
+}
---
+++ src-be/04.Logics/Member/Polls/Commands/UpdatePoll/UpdatePollCommand.cs
@@ -0,0 +1,72 @@
+﻿using Delta.Polling.Base.Polls.Enums;
+using Delta.Polling.Both.Member.Polls.Commands.UpdatePoll;
+
+namespace Delta.Polling.Logics.Member.Polls.Commands.UpdatePoll;
+
+[Authorize(RoleName = RoleNameFor.Member)]
+public class UpdatePollCommand : UpdatePollRequest, IRequest
+{
+}
+
+public class UpdatePollCommandValidator : AbstractValidator<UpdatePollCommand>
+{
+    public UpdatePollCommandValidator()
+    {
+        Include(new UpdatePollRequestValidator());
+    }
+}
+
+public class UpdatePollCommandHandler(
+    IDatabaseService databaseService,
+    ICurrentUserService currentUserService)
+    : IRequestHandler<UpdatePollCommand>
+{
+    public async Task Handle(UpdatePollCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(currentUserService.Username))
+        {
+            throw new Exception("User is not authenticated.");
+        }
+
+        var poll = await databaseService.Polls
+                    .Where(p => p.Id == request.PollId)
+                    .SingleOrDefaultAsync(cancellationToken)
+                    ?? throw new EntityNotFoundException("Poll", request.PollId);
+
+        if (poll.Status is not PollStatus.Draft)
+        {
+            throw new ForbiddenException($"Can't update poll with status that is not draft");
+        }
+
+        var memberGroup = await databaseService.GroupMembers
+                        .Where(gm => gm.GroupId == poll.GroupId)
+                        .Select(gm => gm.Username)
+                        .ToListAsync(cancellationToken);
+
+        var isInGroup = memberGroup
+            .Any(member =>
+            {
+                return member == currentUserService.Username;
+            });
+
+        if (!isInGroup)
+        {
+            throw new ForbiddenException($"Can't update poll: {request.PollId}, because you are not member of this group");
+        }
+
+        if (poll.CreatedBy != currentUserService.Username)
+        {
+            throw new ForbiddenException($"Can't update poll: {request.PollId}, because this is not your poll");
+        }
+
+        poll.Modified = DateTimeOffset.Now;
+        poll.ModifiedBy = currentUserService.Username;
+        poll.AllowOtherChoice = request.AllowOtherChoice;
+        poll.MaximumAnswer = request.MaximumAnswer;
+        poll.AllowOtherChoice = request.AllowOtherChoice;
+        poll.Title = request.Title;
+        poll.Question = request.Question;
+
+        _ = await databaseService.SaveAsync(cancellationToken);
+    }
+}
