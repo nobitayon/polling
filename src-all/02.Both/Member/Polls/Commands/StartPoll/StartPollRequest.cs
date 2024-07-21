396d4c0ffdb053f76f5db500e05b0afe1273bd11
nobitayon <yonrungrung@gmail.com>
2024-07-22 05:13:16 +0700

    membuat logic untuk start poll
    #258

---
+++ src-all/02.Both/Member/Polls/Commands/StartPoll/StartPollRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Delta.Polling.Both.Member.Polls.Commands.StartPoll;
+
+public class StartPollRequest
+{
+    public required Guid PollId { get; init; }
+}
+
+public class StartPollRequestValidator : AbstractValidator<StartPollRequest>
+{
+    public StartPollRequestValidator()
+    {
+        _ = RuleFor(x => x.PollId)
+           .NotEmpty();
+    }
+}
