1b50ad87cf6a62e5461aeeac1dc6dbca10f4762c
nobitayon <yonrungrung@gmail.com>
2024-07-22 07:50:32 +0700

    membuat logic untuk add member ke suatu grup
    - 1 request menerima list username untuk ditambah ke suatu grup
    
    #265

---
+++ src-all/02.Both/Admin/Groups/Commands/AddMember/AddMemberRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace Delta.Polling.Both.Admin.Groups.Commands.AddMember;
+
+public class AddMemberRequest
+{
+    public required Guid GroupId { get; init; }
+    public required List<string> ListMemberUsername { get; init; }
+}
+
+public class AddMemberRequestValidator : AbstractValidator<AddMemberRequest>
+{
+    public AddMemberRequestValidator()
+    {
+        _ = RuleFor(x => x.ListMemberUsername)
+            .Must(x => x != null && x.Count >= 1)
+           .WithMessage("ListMemberUsername must contain more than one item.");
+
+        _ = RuleFor(x => x.GroupId)
+            .NotEmpty();
+    }
+}
---
+++ src-be/04.Logics/Admin/Groups/Commands/AddMember/AddMemberCommand.cs
@@ -0,0 +1,58 @@
+﻿using Delta.Polling.Both.Admin.Groups.Commands.AddMember;
+using Delta.Polling.Domain.Groups.Entities;
+
+namespace Delta.Polling.Logics.Admin.Groups.Commands.AddMember;
+
+[Authorize(RoleName = RoleNameFor.Administrator)]
+public class AddMemberCommand : AddMemberRequest, IRequest<AddMemberOutput>
+{
+}
+
+public class AddMemberCommandValidator : AbstractValidator<AddMemberCommand>
+{
+    public AddMemberCommandValidator()
+    {
+        Include(new AddMemberRequestValidator());
+    }
+}
+
+public class AddMemberCommandHandler(
+    IDatabaseService databaseService,
+    ICurrentUserService currentUserService)
+    : IRequestHandler<AddMemberCommand, AddMemberOutput>
+{
+    public async Task<AddMemberOutput> Handle(AddMemberCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(currentUserService.Username))
+        {
+            throw new Exception("User is not authenticated.");
+        }
+
+        var listGroupMember = new List<Guid>();
+        foreach (var username in request.ListMemberUsername)
+        {
+            var groupMember = new GroupMember
+            {
+                GroupId = request.GroupId,
+                Username = username,
+                Created = DateTimeOffset.Now,
+                CreatedBy = currentUserService.Username
+            };
+
+            _ = await databaseService.GroupMembers.AddAsync(groupMember, cancellationToken);
+
+            listGroupMember.Add(groupMember.Id);
+        }
+
+        _ = await databaseService.SaveAsync(cancellationToken);
+
+        return new AddMemberOutput
+        {
+            Data = new AddMemberResult
+            {
+                ListGroupMemberId = listGroupMember
+            }
+        };
+    }
+}
+
